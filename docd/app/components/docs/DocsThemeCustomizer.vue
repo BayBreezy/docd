dc653bb4b8a3df2f6d348d1ef189a52c2971afed
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2026-04-26 14:26:17 -0500

    chore: wrap theme customizer in ClientOnly to get rid of hydration mismatch

--- docd/app/components/docs/DocsThemeCustomizer.vue
+++ docd/app/components/docs/DocsThemeCustomizer.vue
@@ -1,41 +1,48 @@
 <template>
-  <UiPopover v-if="isDesktop">
-    <UiTooltip>
-      <UiTooltipTrigger as-child>
-        <UiPopoverTrigger as-child>
-          <UiButton size="icon-sm" variant="ghost">
-            <Icon name="lucide:palette" />
-          </UiButton>
-        </UiPopoverTrigger>
-      </UiTooltipTrigger>
-      <UiPopoverContent
-        translucent
-        class="w-auto overflow-y-auto p-4"
-        side="right"
-        align="end"
-        :side-offset="8"
-      >
-        <DocsThemeCustomizerCore />
-      </UiPopoverContent>
-      <UiTooltipContent> Customize theme </UiTooltipContent>
-    </UiTooltip>
-  </UiPopover>
-  <UiDrawer v-else>
-    <UiDrawerTrigger as-child>
-      <UiButton size="icon-sm" variant="ghost">
+  <ClientOnly>
+    <template #fallback>
+      <UiButton size="icon-sm" variant="ghost" disabled>
         <Icon name="lucide:palette" />
       </UiButton>
-    </UiDrawerTrigger>
-    <UiDrawerContent class="w-full px-4 pb-6">
-      <UiDrawerHeader>
-        <UiDrawerTitle class="sr-only">Theme Customizer</UiDrawerTitle>
-        <UiDrawerDescription class="sr-only"
-          >Pick a style and color for your components.</UiDrawerDescription
+    </template>
+    <UiPopover v-if="isDesktop">
+      <UiTooltip>
+        <UiTooltipTrigger as-child>
+          <UiPopoverTrigger as-child>
+            <UiButton size="icon-sm" variant="ghost">
+              <Icon name="lucide:palette" />
+            </UiButton>
+          </UiPopoverTrigger>
+        </UiTooltipTrigger>
+        <UiPopoverContent
+          translucent
+          class="w-auto overflow-y-auto p-4"
+          side="right"
+          align="end"
+          :side-offset="8"
         >
-      </UiDrawerHeader>
-      <DocsThemeCustomizerCore />
-    </UiDrawerContent>
-  </UiDrawer>
+          <DocsThemeCustomizerCore />
+        </UiPopoverContent>
+        <UiTooltipContent> Customize theme </UiTooltipContent>
+      </UiTooltip>
+    </UiPopover>
+    <UiDrawer v-else>
+      <UiDrawerTrigger as-child>
+        <UiButton size="icon-sm" variant="ghost">
+          <Icon name="lucide:palette" />
+        </UiButton>
+      </UiDrawerTrigger>
+      <UiDrawerContent class="w-full px-4 pb-6">
+        <UiDrawerHeader>
+          <UiDrawerTitle class="sr-only">Theme Customizer</UiDrawerTitle>
+          <UiDrawerDescription class="sr-only"
+            >Pick a style and color for your components.</UiDrawerDescription
+          >
+        </UiDrawerHeader>
+        <DocsThemeCustomizerCore />
+      </UiDrawerContent>
+    </UiDrawer>
+  </ClientOnly>
 </template>
 
 <script lang="ts" setup>
